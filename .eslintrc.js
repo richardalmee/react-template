544f0e047fb5680192ca3bfa45ea9ffed0686600
richardalmee <come2fouch@gmail.com>
2020-04-21 12:00:16 +0200

    more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-  extends: ['codingitwrong'],
+  extends: ['airbnb', 'prettier'],
   parser: 'babel-eslint',
-  plugins: ['cypress', 'jest', 'react'],
+  plugins: ['cypress', 'jest', 'react', 'prettier'],
   env: {
-    node: true,
-    browser: true,
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true,
     'jest/globals': true,
     'cypress/globals': true,
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn',
   },
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const root = document.getElementById('root');
-
 ReactDOM.render(<App />, root);
